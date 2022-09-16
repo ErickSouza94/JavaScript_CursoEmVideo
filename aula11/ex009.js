aa46dd3d0c93b22681aec02fc50cda5e40ac7b66
Erick Souza <erick.ssouza94@gmail.com>
2022-09-16 14:54:37 -0300

    Desafio ex009 JS

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+/* Condição Composta */
+/* FAZER O DESAFIO DESSE PROGRAMA EM HTML */
+var país = 'Portugal'
+if (país == 'Brasil'){
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
