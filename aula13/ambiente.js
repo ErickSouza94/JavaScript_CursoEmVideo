d7b0dea19b0a368c453a589647d99ffc3f4f8926
Erick Souza <erick.ssouza94@gmail.com>
2022-09-21 19:27:21 -0300

    aula14

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+var b = 1
+while (b <= 6) {
+    console.log(`Passo b ${b}`)
+    b++ // b = b + 1
+}
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
